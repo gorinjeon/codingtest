9c7a3d84436909129b57b3557ba66cbefbe72855
gorinjeon <105953963+gorinjeon@users.noreply.github.com>
2022-05-21 17:25:00 +0900

    Update 2753. 윤년.py

--- BOJ/단계별/2. 조건문/2753. 윤년.py
+++ BOJ/단계별/2. 조건문/2753. 윤년.py
@@ -12,3 +12,13 @@ elif a%100 == 0:
 elif a%4 == 0: 
   print(1)
 else: print(0)
+
+# 다른 사람의 풀이 
+a = int(input())
+if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
+    print(1)
+else:
+    print(0)
+    
+year = int(input())
+print('1') if ((year%4 == 0)and(year%100 != 0)) or (year%400 == 0) else print('0')
