87bad6ec062cfd0651ba9ff15c8582c8e21e34b7
gorinjeon <105953963+gorinjeon@users.noreply.github.com>
2022-05-21 16:47:50 +0900

    Update 2588. 곱셈.py

--- BOJ/단계별/01. 입출력과 사칙연산/2588. 곱셈.py
+++ BOJ/단계별/01. 입출력과 사칙연산/2588. 곱셈.py
@@ -6,13 +6,11 @@ print(a*((b%100)//10))
 print(a*(b//100))
 print(a*b)
 
-# 다른 사람의 풀이: https://imototot.tistory.com/194
-a = int(input())
-b = input()
+# 다른 사람의 풀이: https://like-a-happy-cat.tistory.com/10
+A = int(input())
+B = input()
 
-a1 = a * int(b[2])
-a2 = a * int(b[1])
-a3 = a * int(b[0])
-a4 = a * int(b)
-
-print (a1, a2, a3, a4, sep='\n')
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*int(B))
