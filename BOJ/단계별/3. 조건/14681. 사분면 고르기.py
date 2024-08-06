01d87e3f0a54f9f29db5b3efd91667c58df1d83c
gorinjeon <105953963+gorinjeon@users.noreply.github.com>
2024-08-06 10:05:30 +0900

    Create 14681. 사분면 고르기.py

---
+++ BOJ/단계별/3. 조건/14681. 사분면 고르기.py
@@ -0,0 +1,15 @@
+x = int(input())
+y = int(input())
+
+if x > 0:
+    if y > 0: 
+        print(1)
+    else: 
+        print(4)
+else:
+    if y > 0:
+        print(2)
+    else: 
+        print(3)
+
+a,b=open(0);print('3421'[a>'0'::2][b>'0'])
