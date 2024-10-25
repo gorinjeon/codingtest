c2930915c98ac3d49a8bc6d07dfb5a145af83ab8
gorinjeon <105953963+gorinjeon@users.noreply.github.com>
2024-10-25 14:27:09 +0900

    Create 소인수분해.py

---
+++ Programmers/코딩테스트입문/소인수분해.py
@@ -0,0 +1,12 @@
+# 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+def solution(n):
+    factors, i = set(), 2
+    while i * i <= n:
+        if n % i == 0:
+            factors.add(i)
+            n //= i
+        else:
+            i += 1
+    if n > 1:
+        factors.add(n)
+    return sorted(factors)
